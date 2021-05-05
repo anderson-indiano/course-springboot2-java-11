4d3f2579904000fac0b1192089baa2384d1f4abf
anderson-indiano <indianobox2@gmail.com>
2021-05-05 17:22:21 -0300

    Exception handling - findById

---
+++ src/main/java/com/educandoweb/workshop/controllers/exceptions/StandardError.java
@@ -0,0 +1,29 @@
+package com.educandoweb.workshop.controllers.exceptions;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class StandardError implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@JsonFormat(shape = Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",timezone = "GMT")
+	@Getter @Setter private Instant instant;
+	@Getter @Setter private Integer status;
+	@Getter @Setter private String error;
+	@Getter @Setter private String message;
+	@Getter @Setter private String path;
+	
+}
